ce7aa7eca10822fdb86039a9942ca9f996120338
sandeshreads <sandeshreads@gmail.com>
2020-09-12 23:38:17 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Boo ya!</p>
+        <p>Boo ya, female dogs!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
